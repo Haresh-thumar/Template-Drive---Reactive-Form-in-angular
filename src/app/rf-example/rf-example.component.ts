86810ff742dd84e31e4f1b97805db1a9305ab6d7
Haresh-thumar <hareshthumar46@gmail.com>
2023-08-06 21:56:32 +0530

    reactive form example logic

--- src/app/rf-example/rf-example.component.ts
+++ src/app/rf-example/rf-example.component.ts
@@ -38,6 +38,7 @@ export class RfExampleComponent {
   }
 
   ngAfterViewInit(): void {
+    /*============ for dob ============*/
     this.studentForm.controls['dob'].valueChanges.subscribe(res => {
       console.log(res);
       /*----- calculate age ------*/
@@ -49,6 +50,24 @@ export class RfExampleComponent {
       /*----- Set Required Validation if Age is 18 plus ------*/
       if (finalAge > 18) {
         this.studentForm.controls['licenceNo'].addValidators(Validators.required);
+      } else {
+        this.studentForm.controls['licenceNo'].removeValidators(Validators.required);
+      }
+    });
+    /*============ for country ============*/
+    this.studentForm.controls['country'].valueChanges.subscribe(res => {
+      if (res === 'India') {
+        this.studentForm.controls['state'].addValidators(Validators.required);
+      } else {
+        this.studentForm.controls['state'].removeValidators(Validators.required);
+      }
+    });
+    /*============ for Identity Card ============*/
+    this.studentForm.controls['identityType'].valueChanges.subscribe(res => {
+      if (res === 'aadharCard') {
+        this.studentForm.controls['cardno'].setValidators(Validators.pattern('^[A-Za-z]{5}[0-9]{4}[A-Za-z]$'));
+      } else {
+        this.studentForm.controls['cardNo'].setValidators(Validators.pattern('^[2-9]{1}[0-9]{3}\\s[0-9]{4}\\s[0-9]{4}$'));
       }
     });
   }
@@ -57,6 +76,11 @@ export class RfExampleComponent {
     /*------ Join the Full Name ------*/
     const fullName = this.studentForm.controls['firstname'].value + ' ' + this.studentForm.controls['middlename'].value + ' ' + this.studentForm.controls['lastname'].value;
     this.studentForm.controls['fullName'].setValue(fullName);
+    if (this.studentForm.controls['fullName'].value.trim() !== '') {
+      this.studentForm.controls['fullName'].disable();
+    } else {
+      this.studentForm.controls['fullName'].enable();
+    }
   }
 
 }
